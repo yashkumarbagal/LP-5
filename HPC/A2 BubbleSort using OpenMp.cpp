92cc7f4e0d90c37130dcc0f41ec802a658cbe7e4
Yashkumar Bagal <126667153+yashkumarbagal@users.noreply.github.com>
2025-05-08 22:54:14 +0530

    Update A2 BubbleSort using OpenMp.cpp

--- HPC/A2 BubbleSort using OpenMp.cpp
+++ HPC/A2 BubbleSort using OpenMp.cpp
@@ -1,52 +1,44 @@
-#include<iostream> 
-#include<stdlib.h> 
-#include<omp.h>
+#include <iostream>
+#include <stdlib.h>
+#include <omp.h>
+#include <ctime>     // For clock()
+#include <iomanip>   // For setprecision
 
 using namespace std;
 
-void bubble_sequential(int *, int); 
-void bubble_parallel(int *, int); 
-void swap(int &, int &); 
+void bubble_sequential(int *, int);
+void bubble_parallel(int *, int);
+void swap(int &, int &);
 
-void bubble_sequential(int *a, int n) 
-{ 
-    for (int i = 0; i < n; i++) 
-    {     
-        for (int j = 0; j < n - 1; j++) 
-        {     
-            if (a[j] > a[j + 1]) 
-            {     
-                swap(a[j], a[j + 1]); 
-            }     
-        }     
-    } 
+void bubble_sequential(int *a, int n)
+{
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n - 1; j++)
+            if (a[j] > a[j + 1])
+                swap(a[j], a[j + 1]);
 }
 
-void bubble_parallel(int *a, int n) 
-{ 
-    for (int i = 0; i < n; i++) 
-    {     
-        int first = i % 2;    
-        #pragma omp parallel for shared(a, first) 
-        for (int j = first; j < n - 1; j += 2) 
-        {     
-            if (a[j] > a[j + 1]) 
-            {     
-                swap(a[j], a[j + 1]); 
-            }     
-        }     
-    } 
+void bubble_parallel(int *a, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        int first = i % 2;
+#pragma omp parallel for shared(a, first)
+        for (int j = first; j < n - 1; j += 2)
+            if (a[j] > a[j + 1])
+                swap(a[j], a[j + 1]);
+    }
 }
 
-void swap(int &a, int &b) 
-{ 
-    int temp = a; 
-    a = b; 
-    b = temp; 
-} 
+void swap(int &a, int &b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
 
-int main() 
-{ 
+int main()
+{
     int *a, n, choice;
 
     cout << "\nChoose the sorting method: ";
@@ -55,35 +47,54 @@ int main()
     cout << "\nEnter your choice: ";
     cin >> choice;
 
-    if (choice == 1 || choice == 2) {
-        cout << "\nEnter total number of elements: "; 
-        cin >> n; 
+    if (choice == 1 || choice == 2)
+    {
+        cout << "\nEnter total number of elements: ";
+        cin >> n;
 
-        a = new int[n]; 
+        a = new int[n];
 
-        cout << "\nEnter elements: "; 
-        for (int i = 0; i < n; i++) 
-        { 
-            cin >> a[i]; 
-        }
+        // Auto-fill with random values
+        srand(time(0));
+        for (int i = 0; i < n; i++)
+            a[i] = rand() % 1000;
+
+        // Print unsorted array
+        cout << "\nUnsorted array: ";
+        for (int i = 0; i < n; i++)
+            cout << a[i] << " ";
+        cout << endl;
+
+        // Start timing
+        clock_t start_time = clock();
 
         if (choice == 1)
             bubble_sequential(a, n);
         else
             bubble_parallel(a, n);
 
-        cout << "\nSorted array: "; 
-        for (int i = 0; i < n; i++) 
-        { 
-            cout << a[i] << " "; 
-        }
+        // End timing
+        clock_t end_time = clock();
+
+        // Calculate time in microseconds
+        double time_microsec = 1.0 * (end_time - start_time) * 1e6 / CLOCKS_PER_SEC;
+
+        // Print sorted array
+        cout << "\nSorted array: ";
+        for (int i = 0; i < n; i++)
+            cout << a[i] << " ";
         cout << endl;
 
+        // Display execution time
+        cout << fixed << setprecision(2);
+        cout << "Execution time: " << time_microsec << " microseconds" << endl;
+
         delete[] a;
-    } else {
+    }
+    else
+    {
         cout << "Invalid choice! Exiting program..." << endl;
     }
 
-    return 0; 
+    return 0;
 }
-
